ea31d53a3373c431f3de8c50b47e1f237dbd9cac
Nitheshwaran <nitheshwaran003@gmail.com>
2024-06-19 12:19:33 +0530

    permnentLeft drawer

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,21 @@ import './App.css'
 import MiniDrawer from './Component/Side-Bar/Minivariantdrawer'
 import CF_Autocomplete from './Component/InputFields/CF_Autocomplete'
 import { Routes, Route } from 'react-router-dom';
+import PermanentDrawerLeft from './Component/Side-Bar/PermanentDrawerLeft';
 
 function App() {
 
   return (
     <>
-    <Routes>
+      <Routes>
+        
+        {/* Drawer */}
         <Route path="mini-drawer" element={<MiniDrawer />} />
+        <Route path="/permanentLeftDrawer" element={<PermanentDrawerLeft />} />
+
+        {/* Input */}
         <Route path="/autocomplete" element={<CF_Autocomplete />} />
+
       </Routes>
     </>
   )
